61f7aa3fec4c721a6af228b8cb6cdfc7140bb0f7
MesseBoris <essem0409@gmail.com>
2018-03-23 12:39:58 +0000

    Déplacemement login dans le lien par défault

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -115,34 +115,6 @@ class Ticket
 		return $this;
     }
 	
-	/**
-	 * @ORM\Column(type="text", length=255, nullable=true)
-	 */
-	private $prequalification;
-	
-	/**
-	 * Get prequalification
-	 *
-	 *@return string
-	 */
-	public function getPrequalification()
-    {
-        return $this->prequalification;
-    }
-
-	/**
-	 * Set prequalification
-	 *
-	 *@param string $prequalification
-	 *
-	 *@return Ticket
-	 */
-    public function setPrequalification($qual)
-    {
-        $this->prequalification = $qual;
-		
-		return $this;
-    }
 	
 	
 	/**
